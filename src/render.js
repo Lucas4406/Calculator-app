ed0c69959079dab8f43814a5dfc952e3cfa8a483
Lucas <lucas.popescu41@gmail.com>
2022-06-28 14:38:49 +0300

    s-a rezolvat

--- src/render.js
+++ src/render.js
@@ -133,3 +133,19 @@ document.getElementById('toggle-dark-mode').addEventListener('click', async()=>{
     const isDarkMode=await window.darkMode.toggle()
     document.getElementById('theme-source').innerHTML=isDarkMode ? 'Dark' : 'Light'
 })
+
+const closeBtn = document.getElementById("close-button")
+const minBtn = document.getElementById("minimize-button")
+const maxBtn = document. getElementById("maximize-button")
+closeBtn.addEventListener("click" , closeApp)
+minBtn.addEventListener("click", minApp)
+maxBtn.addEventListener("click" , maxApp)
+function closeApp(){
+    app.window.close()
+}
+function minApp(){
+    app.window.minimize()
+}
+function maxApp(){
+    app.window.maximize()
+}
